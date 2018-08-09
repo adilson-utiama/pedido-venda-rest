fa4ae572dc0c94ba1c782d267ce6fbf117501869
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-09 16:16:46 -0300

    Mapeamento entre Categoria e Produto

--- src/main/java/com/asuprojects/pvconceitual/domain/Produto.java
+++ src/main/java/com/asuprojects/pvconceitual/domain/Produto.java
@@ -5,14 +5,30 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private BigDecimal preco;
 
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn(name = "produto_id"),
+			inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
 	private List<Categoria> categorias = new ArrayList<>();
 
 	public Produto() {}
