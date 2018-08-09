870be7c255c899ece22ee7daf9699ed8e257474a
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-09 13:05:09 -0300

    Commit Inicial

---
+++ src/main/java/com/asuprojects/pvconceitual/PvConceitualApplication.java
@@ -0,0 +1,12 @@
+package com.asuprojects.pvconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvConceitualApplication.class, args);
+	}
+}
