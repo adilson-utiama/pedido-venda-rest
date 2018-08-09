923d1bb90569b2921121b97ead84626697455556
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-09 16:03:57 -0300

    criacao classe Produto

--- src/main/java/com/asuprojects/pvconceitual/domain/Produto.java
+++ src/main/java/com/asuprojects/pvconceitual/domain/Produto.java
@@ -1,31 +1,26 @@
 package com.asuprojects.pvconceitual.domain;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
-	private List<Produto> produtos = new ArrayList<>();
-	
-	public Categoria() {}
+	private BigDecimal preco;
 
-	public Categoria(Integer id, String nome) {
+	private List<Categoria> categorias = new ArrayList<>();
+
+	public Produto() {}
+
+	public Produto(Integer id, String nome, BigDecimal preco) {
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -43,13 +38,21 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	public List<Produto> getProdutos() {
-		return produtos;
+
+	public BigDecimal getPreco() {
+		return preco;
 	}
-	
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -57,7 +60,6 @@ public class Categoria implements Serializable {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
 
@@ -69,20 +71,13 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
 		return true;
 	}
-	
-	
 
 }
