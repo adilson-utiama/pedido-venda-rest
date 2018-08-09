6595c9612cfd16a25b21c4704ac150c09009c251
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-09 16:26:01 -0300

    Criacao ProdutoRepository e populando no banco H2

--- src/main/java/com/asuprojects/pvconceitual/PvConceitualApplication.java
+++ src/main/java/com/asuprojects/pvconceitual/PvConceitualApplication.java
@@ -1,5 +1,6 @@
 package com.asuprojects.pvconceitual;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.asuprojects.pvconceitual.domain.Categoria;
+import com.asuprojects.pvconceitual.domain.Produto;
 import com.asuprojects.pvconceitual.repositories.CategoriaRepository;
+import com.asuprojects.pvconceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PvConceitualApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categorias;
+	@Autowired
+	private ProdutoRepository produtos;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PvConceitualApplication.class, args);
@@ -26,7 +31,20 @@ public class PvConceitualApplication implements CommandLineRunner{
 		//Apenas para teste, populando banco ao startar spring boot
 		Categoria c1 = new Categoria(null, "Informatica");
 		Categoria c2 = new Categoria(null, "Escritorio");
+		
+		Produto p1 = new Produto(null, "Computador", new BigDecimal(2000.0));
+		Produto p2 = new Produto(null, "Impressora", new BigDecimal(800.0));
+		Produto p3 = new Produto(null, "Mouse", new BigDecimal(80.0));
+		
+		c1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().add(c1);
+		p2.getCategorias().addAll(Arrays.asList(c1, c2));
+		p3.getCategorias().add(c1);
+		
 		categorias.saveAll(Arrays.asList(c1, c2));
+		produtos.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 }
