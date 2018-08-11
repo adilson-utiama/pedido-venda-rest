6a989a78ae2353a522cbca2cbe1a844c8c7f1152
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-11 16:37:46 -0300

    Ajuste ao deletar Cliente sem Pedidos

--- src/main/java/com/asuprojects/pvconceitual/resources/ClienteResource.java
+++ src/main/java/com/asuprojects/pvconceitual/resources/ClienteResource.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.asuprojects.pvconceitual.domain.Categoria;
 import com.asuprojects.pvconceitual.domain.Cliente;
 import com.asuprojects.pvconceitual.dto.ClienteDTO;
 import com.asuprojects.pvconceitual.dto.ClienteNewDTO;
