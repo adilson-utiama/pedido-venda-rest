aa5b0ea67f1b498c21d7ff8267737f75fb4fd5d9
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-10 21:25:24 -0300

    Deletando recurso Categoria com DELETE

--- src/main/java/com/asuprojects/pvconceitual/services/CategoriaService.java
+++ src/main/java/com/asuprojects/pvconceitual/services/CategoriaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.asuprojects.pvconceitual.domain.Categoria;
 import com.asuprojects.pvconceitual.repositories.CategoriaRepository;
+import com.asuprojects.pvconceitual.services.exceptions.DataIntegrityException;
 import com.asuprojects.pvconceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,6 +33,15 @@ public class CategoriaService {
 		findById(categoria.getId());
 		return categorias.save(categoria);
 	} 
+	
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			categorias.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir categoria que possui produttos");
+		}
+	}
 	
 	public List<Categoria> findAll() {
 		return categorias.findAll();
