095ed1382320b234114e0bedc1f6c2c72c3f51b3
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-11 17:58:52 -0300

    Validacao customizada de CPF e CNPJ na insercao do Cliente

---
+++ src/main/java/com/asuprojects/pvconceitual/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,44 @@
+package com.asuprojects.pvconceitual.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.asuprojects.pvconceitual.domain.enums.TipoCliente;
+import com.asuprojects.pvconceitual.dto.ClienteNewDTO;
+import com.asuprojects.pvconceitual.resources.exceptions.FieldMessage;
+import com.asuprojects.pvconceitual.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && 
+				!BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Invalido."));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && 
+				!BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Invalido."));
+		}
+		
+		// inclua os testes aqui, inserindo erros na lista
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
