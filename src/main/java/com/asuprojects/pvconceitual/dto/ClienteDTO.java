d9792ebd24bbeac940d0159d7cbed851384153e6
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-11 18:50:50 -0300

    Validacao customizada de email repetido para atualizacao Cliente

--- src/main/java/com/asuprojects/pvconceitual/dto/ClienteDTO.java
+++ src/main/java/com/asuprojects/pvconceitual/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.asuprojects.pvconceitual.domain.Cliente;
+import com.asuprojects.pvconceitual.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
