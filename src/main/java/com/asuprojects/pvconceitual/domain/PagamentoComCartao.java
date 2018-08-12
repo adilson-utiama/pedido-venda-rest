c8471dbac1dda6f58f7ec22803da9aa4860eae15
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-12 00:19:17 -0300

    Permitido a instanciação de subclasses de Pagamento a partir de dados JSON

--- src/main/java/com/asuprojects/pvconceitual/domain/PagamentoComCartao.java
+++ src/main/java/com/asuprojects/pvconceitual/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.asuprojects.pvconceitual.domain;
 import javax.persistence.Entity;
 
 import com.asuprojects.pvconceitual.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 
 	private static final long serialVersionUID = 1L;
