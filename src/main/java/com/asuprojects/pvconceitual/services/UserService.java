d4b63a3de978b4c840aa9944e052405a31f5642b
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-20 23:54:58 -0300

    Criando um UserService para retornar usuario logado

---
+++ src/main/java/com/asuprojects/pvconceitual/services/UserService.java
@@ -0,0 +1,16 @@
+package com.asuprojects.pvconceitual.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.asuprojects.pvconceitual.security.UserSS;
+
+public class UserService {
+
+	public static UserSS authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
