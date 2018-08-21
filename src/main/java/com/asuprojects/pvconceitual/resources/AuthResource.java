317c008c38cd55dba1f03727734db94b7adb4cca
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-21 13:50:38 -0300

    Refresh token

---
+++ src/main/java/com/asuprojects/pvconceitual/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.asuprojects.pvconceitual.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.asuprojects.pvconceitual.security.UserSS;
+import com.asuprojects.pvconceitual.security.jwt.JwtUtil;
+import com.asuprojects.pvconceitual.services.UserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	@PostMapping("/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
--- src/main/java/com/asuprojects/pvconceitual/resources/PedidoResource.java
+++ src/main/java/com/asuprojects/pvconceitual/resources/PedidoResource.java
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.asuprojects.pvconceitual.domain.Categoria;
 import com.asuprojects.pvconceitual.domain.Pedido;
-import com.asuprojects.pvconceitual.dto.CategoriaDTO;
 import com.asuprojects.pvconceitual.services.PedidoService;
 
 @RestController
